0b0c4825559e2bfc59e95487d69338ad612df15b
Leah Lundqvist <leah@pigeon.sh>
2024-05-24 20:27:09 +0200

    lint source files

--- src/queue.ts
+++ src/queue.ts
@@ -6,21 +6,21 @@ type Instance = {
 function StartInstances(queue: WebSocket[]) {
 	if (queue.length < 2) return;
 	// Create array of lobby instances
-	let lobbies = [];
+	const lobbies = [];
 	while (queue.length >= 2) {
-		let player1 = queue.shift();
-		let player2 = queue.shift();
+		const player1 = queue.shift();
+		const player2 = queue.shift();
 		lobbies.push([player1, player2]);
 	}
 	// Start instances
-	lobbies.forEach((lobby) => {
+	for (const lobby of lobbies) {
 		GenerateInstance().then((instance: Instance) => {
 			if (lobby[0] && lobby[1]) {
-				lobby[0].send("start:" + instance.address + ":" + instance.port);
-				lobby[1].send("start:" + instance.address + ":" + instance.port);
+				lobby[0].send(`start:${instance.address}:${instance.port}`);
+				lobby[1].send(`start:${instance.address}:${instance.port}`);
 			}
 		});
-	});
+	}
 }
 
 function GenerateInstance(): Promise<Instance> {
