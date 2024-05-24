29f8c8531f0a54be356055215977de35533aa2a6
cmkrist <cmkrist@umich.edu>
2024-05-24 10:13:01 -0400

    Basic WS and API

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,89 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { StartInstances } from "./queue";
+enum STATUS {
+  ERR,
+  OK,
+  FORBIDDEN,
+  MISSING
+}
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+
+let Queue: any[] = [];
+const QueueThread = new Worker(new URL("./queue.ts", import.meta.url));
+
+// Routes
+app.get("/", () => "Hello Elysia");
+
+app.get("/connect", () => {
+  return {
+    status: STATUS.FORBIDDEN,
+    error: {
+      code: STATUS.ERR,
+      message: "This is for websocket connections"
+    }
+  }
+})
+
+// With Types
+app.get("/connect", {
+  response: t.Object({
+    status: t.Integer()
+  }),
+  handler() {
+    return {
+      status: STATUS.FORBIDDEN,
+      error: {
+        code: STATUS.ERR,
+        message: "This is for websocket connections"
+      }
+    }
+  }
+})
+// WS
+app.ws('/ws', {
+  open(ws) {
+    console.log(`User connectd: ${ws.id}`)
+  },
+  close(ws) {
+    console.log(`User disconnected: ${ws.id}`)
+  },
+  message(ws, message) {
+    switch (message) {
+      // Check RTT
+      case "ping":
+        ws.send("pong");
+        break;
+      // Join Queue
+      case "queue":
+        if (Queue.includes(ws)) return;
+        Queue.push(ws);
+        ws.send("queued");
+        break;
+      // Leave Queue
+      case "leave":
+        Queue = Queue.filter((user) => user !== ws);
+        ws.send("left");
+        break;
+      // Accept Queue Pop
+      case "accept":
+        // Accept the user
+        if (Queue[0] === ws) {
+          ws.send("accepted");
+          Queue.shift();
+        }
+        break;
+      default:
+        ws.send("Invalid Command");
+    }
+  }
+})
+
+app.listen(Bun.env.PORT || 8080)
+
+setInterval(() => {
+  StartInstances(Queue);
+}, 1000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
---
+++ src/queue.ts
@@ -0,0 +1,37 @@
+type Instance = {
+    port: number;
+    address: string;
+}
+
+function StartInstances(queue: WebSocket[]) {
+    if(queue.length < 2) return;
+    // Create array of lobby instances
+    let lobbies = [];
+    while(queue.length >= 2) {
+        let player1 = queue.shift();
+        let player2 = queue.shift();
+        lobbies.push([player1, player2]);
+    }
+    // Start instances
+    lobbies.forEach((lobby) => {
+        GenerateInstance().then((instance:Instance) => {
+            if (lobby[0] && lobby[1]) {
+                lobby[0].send("start:" + instance.address + ":" + instance.port);
+                lobby[1].send("start:" + instance.address + ":" + instance.port);
+            }
+        })
+    });
+}
+
+function GenerateInstance() : Promise<Instance>{
+    return new Promise((res, rej)=>{
+        // Generate instance
+        res({
+            port: 8080,
+            address: ""
+        });
+    })
+}
+
+
+export { StartInstances }
